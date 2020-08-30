f8e3f6512ad3b6a0afbb7aaeae42f8ee1d7f3069
rnkwilliams <rnkwilliams07@gmail.com>
2020-08-30 12:23:03 -0500

    delete files, update title, create Header component, import Header component to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Header from './components/Header'
 import './App.css';
+
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <h1>Hello!</h1>
+        <Header />
       </div>
     )
   }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>The Shoppies Movie Nominations</h1>
+    </header>
+  )
+}
+
+export default Header
