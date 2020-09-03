a16ec48ebfb14c8b103edee3eadb79fe000f08d3
rnkwilliams <rnkwilliams07@gmail.com>
2020-09-03 09:23:06 -0500

    create state, handleChange and handleSubmit functions and pass props to SearchBar component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import Header from './components/Header'
 import SearchBar from './components/SearchBar'
 import './App.css';
 
-
 class App extends React.Component {
+
+  state = {
+    movies: [],
+    searchTerm: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    console.log("in there")
+    e.preventDefault();
+    fetch(`http://www.omdbapi.com/?s=${this.state.searchTerm}&type=movie&apikey=6168123d`)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ movies: [...data.Search] })
+      })
+  }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
+        <SearchBar handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
       </div>
     )
   }
